3082431eca02e840555b9826b0d45992a1144d8c
01zayandev <zayantomara@gmail.com>
2025-11-04 14:26:00 +0800

    unknown

--- admin/new_announcement.php
+++ admin/new_announcement.php
@@ -784,7 +784,7 @@ try {
 
         .form-control {
             width: 100%;
-            padding: 0.75rem 1rem;
+            padding: 0.60rem 1rem;
             border: 1px solid #ddd;
             border-radius: 8px;
             font-size: 1rem;
