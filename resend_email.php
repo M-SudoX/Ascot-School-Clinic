4a3f979648c95bcd9a193853410f31085a533461
01zayandev <zayantomara@gmail.com>
2025-11-20 14:55:28 +0800

    unknown

--- resend_email.php
+++ resend_email.php
@@ -20,8 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
     if ($user && $user['is_verified'] == 0) {
         $fullname = $user['fullname'];
         $activation_code = $user['activation_code'];
-        $activation_link = "http://ascot-onlineservices.net:81/activate.php?code=$activation_code";
-
+        $activation_link = "http://10.244.12.129:8080/ascot-school-clinic/activate.php?code=$activation_code";
         $mail = new PHPMailer(true);
         try {
             $mail->isSMTP();
